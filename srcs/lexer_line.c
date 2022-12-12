e95c46811f21d8ba680bbe8e43581fa0b643f758
tmuramat <mt15hydrangea@gmail.com>
2022-12-13 07:18:28 +0900

    ADD: Lexerの雛形を作成

--- srcs/lexer_line.c
+++ srcs/lexer_line.c
@@ -1,11 +1,37 @@
 #include "minishell.h"
 
+#define INIT_SIZE 36
+
+/** 文字列を
+ * @brief
+ *
+ * @param line
+ * @return t_deque
+ */
+void	scan_line(t_deque *str, char *line)
+{
+	size_t	i;
+
+	i = 0;
+	while (line[i])
+	{
+		ft_deque_push_back(str, line);
+		i++;
+	}
+}
+
 /**
- * @brief 入力された文字列の解析
+ * @brief 入力された文字列を解析（字句解析）する
  *
  * @param line 入力された文字列
  */
 void	lexer_line(char *line)
 {
-	(void)line;
+	t_deque	*str;
+	t_deque *token;
+
+	str = ft_deque_init(sizeof(char *), INIT_SIZE);
+	token = ft_deque_init(sizeof(t_token), INIT_SIZE);
+	scan_line(str, line);
+	//token = tokenizer(str);
 }
