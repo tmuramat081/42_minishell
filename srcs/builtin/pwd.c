e96f2ae3044eec42230d0fdd0c2e3d3fb7c2d418
mmatsuo <mitsuru.matsuo.great@gmail.com>
2022-12-31 18:48:33 +0900

    printfからft_printfに変更

--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -1,37 +1,9 @@
 #include "minishell.h"
-/*
-#include <libc.h>
-#include <stdbool.h>
-typedef struct s_hashmap_data {
-	char	*key;
-	void	*value;
-	bool	in_use;
-}	t_hashmap_data;
 
-typedef struct s_hashmap {
-	size_t			nbits;
-	size_t			mask;
-	t_hashmap_data	*data;
-	size_t			len;
-	size_t			cap;
-	size_t 			(*hash)(const void *);
-}	t_hashmap;
-
-int pwd(char **arg, t_hashmap *map);
-
-int main(void)
+int	pwd(char **arg, t_hashmap *map)
 {
-	char **arg;
-	t_hashmap *map;
-	pwd(arg, map);
-	return (0);
-}
-*/
-
-int pwd(char **arg, t_hashmap *map)
-{
-	char buf[BUFSIZ];
+	char	buf[BUFSIZ];
 	if (getcwd(buf, sizeof(buf)) != NULL)
-		printf("%s\n", buf);
+		ft_printf("%s\n", buf);
 	return (0);
 }
