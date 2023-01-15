f38ce2d6de108c65685cd1cf19719ada3f50f5ed
tmuramat <mt15hydrangea@gmail.com>
2023-01-15 18:32:30 +0900

    ADD: pipelineの実装を一旦プッシュ

--- srcs/execution/exec_line.c
+++ srcs/execution/exec_line.c
@@ -7,15 +7,15 @@
  * @param node
  * @param msh
  */
-void	exec_command_line(t_ast_node *node, t_shell *msh)
+void	exec_command_line(t_ast_node *node, t_process process, t_shell *msh)
 {
 	if (!node)
 		return ;
 	if (node->type & NODE_SEQUENCE)
 	{
-		exec_pipeline(node->left, msh);
-		exec_command_line(node->right, msh);
+		exec_pipeline(node->left, process, msh);
+		exec_command_line(node->right, process, msh);
 		return ;
 	}
-	exec_pipeline(node, msh);
+	exec_pipeline(node, process, msh);
 }
