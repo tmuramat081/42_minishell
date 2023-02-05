7cbd2fb96f4313cfd9e6c8bd8594c322f8a90417
tmuramat <mt15hydrangea@gmail.com>
2023-02-05 21:03:45 +0900

    FIX: exitの返り値を正しく取得するようにした。

--- srcs/builtin/exit.c
+++ srcs/builtin/exit.c
@@ -20,40 +20,42 @@ void	put_error(char *message, char *arg)
 	exit(255);
 }
 
-static int	input_args(char *arg)
+static int	input_args(char **argv)
 {
 	char	*endptr;
 	long	long_num;
 
 	errno = 0;
-	if (!arg)
-		return (0);
 	endptr = NULL;
-	long_num = ft_strtol_d(arg, &endptr);
-	if( (long_num == LONG_MIN || long_num == LONG_MAX) && errno == ERANGE)
+	long_num = ft_strtol_d(argv[1], &endptr);
+	if ((long_num == LONG_MIN || long_num == LONG_MAX) \
+		&& errno == ERANGE)
 	{
-		put_error("numeric argument required", arg);
+		handle_error(MSG_NUM_ARG_REQUIRED, argv[0]);
+		return (255);
 	}
-	else if (*endptr || endptr == arg)
+	else if (*endptr || endptr == argv[1])
 	{
-		put_error("numeric argument required", arg);
+		handle_error(MSG_NUM_ARG_REQUIRED, argv[0]);
+		return (255);
 	}
 	return ((int)long_num);
 }
 
-void	builtin_exit(char **argv)
+int	builtin_exit(char **argv)
 {
-	int		status;
 	size_t 	argc;
 	extern	int g_status;
 
 	argc = ft_matrixlen((const char**)argv);
 	if (argc > 2)
-		handle_error(MSG_TOO_MANY_ARGS, NULL);
-	if (argc == 1)
-		status = input_args(argv[1]);
-	else
-		exit(g_status);
-	exit(status);
+	{
+		handle_error(MSG_TOO_MANY_ARGS, argv[0]);
+		exit(1);
+	}
+	if (argc == 2)
+		exit(input_args(argv));
+	exit (g_status & 0xFF);
+	return (0);
 }
 
