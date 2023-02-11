714d5a4864491276d70d9178928174a4a57d33e6
tmuramat <mt15hydrangea@gmail.com>
2023-02-11 21:35:39 +0900

    Refact: terminalのノーム・ドキュメント対応

--- srcs/terminal/error.c
+++ srcs/terminal/error.c
@@ -1,5 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmuramat <tmuramat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/11 20:28:02 by tmuramat          #+#    #+#             */
+/*   Updated: 2023/02/11 20:29:54 by tmuramat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @file error.c
+ * @brief プログラムに共通で使用するエラーハンドラー関数
+ * @date 2023-02-11
+ */
+
 #include "terminal.h"
 
+/**
+ * @brief プロセスを正しい方法で終了する。
+ *
+ * @detail 子プロセスの場合はexit、親プロセスの場合はreturnを呼び出す。
+ * @param status
+ * @param msh
+ */
 void	maybe_exit(int status, t_shell *msh)
 {
 	extern int	g_status;
@@ -12,12 +37,13 @@ void	maybe_exit(int status, t_shell *msh)
 	exit(status);
 }
 
-/**
- * @brief
+/*
+ * @brief シェルに共通で使用するエラーハンドラー
+ *
  * @detail 仕様上、errnoがEISDIRである場合のみ"Is a directory"を"is a dorectory"に置き換える。
- * @param string　
+ * @param string　エラーの詳細情報（主にコマンド・引数名が入る）
  * @param msh シェルの管理情報
- * @param status
+ * @param status　設定するコマンドの終了ステータス
  */
 void	shell_perror(const char *string, t_shell *msh, int status)
 {
