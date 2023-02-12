47ca856fc40c4ca78b7275868fbbc1fc689239d4
tmuramat <mt15hydrangea@gmail.com>
2023-02-12 17:08:01 +0900

    FIX: cdコマンドのPWD. OLDPWDを正常な挙動に

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: tmuramat <tmuramat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/11 22:22:46 by tmuramat          #+#    #+#             */
-/*   Updated: 2023/02/12 15:20:13 by tmuramat         ###   ########.fr       */
+/*   Updated: 2023/02/12 16:53:29 by tmuramat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,50 @@
  *
  */
 
+#include <limits.h>
 #include "execution.h"
 #include "ft_hashmap.h"
 #include "terminal.h"
 
+static int	update_old_pwd(char *old_path, t_shell *msh)
+{
+	t_env	env;
+
+	env.key = ft_strdup("OLDPWD");
+	env.value = ft_strdup(old_path);
+	return (ft_putenv(&env, msh->envs));
+}
+
+static int	update_new_pwd(t_shell *msh)
+{
+	t_env	env;
+	char	curr_path[PATH_MAX];
+
+	getcwd(curr_path, PATH_MAX);
+	env.key = ft_strdup("PWD");
+	env.value = ft_strdup(curr_path);
+	return (ft_putenv(&env, msh->envs));
+}
+
 int	builtin_cd(char **args, t_shell *msh)
 {
-	char	*path;
+	char	*dst_path;
+	char	curr_path[PATH_MAX];
 
 	if (!args || !*args)
 		return (1);
+	getcwd(curr_path, PATH_MAX);
 	if (!args[1])
-		path = ft_getenv("HOME", msh->envs);
+		dst_path = ft_getenv("HOME", msh->envs);
 	else
-		path = args[1];
-	if (chdir(path))
+		dst_path = args[1];
+	if (chdir(dst_path))
 	{
 		errno = ENOENT;
-		builtin_perror(MSG_NO_FILE_DIR, path, args[0], msh);
+		builtin_perror(MSG_NO_FILE_DIR, dst_path, args[0], msh);
 		return (1);
 	}
+	if (!update_old_pwd(curr_path, msh) || !update_new_pwd(msh))
+		return (-1);
 	return (0);
 }
